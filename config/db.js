e53b8fda6b82c1e4151b0992d85c69ba82014c71
raihank43 <raihankusuma5@gmail.com>
2024-03-26 09:03:36 +0700

    feat: dotenv

--- config/db.js
+++ config/db.js
@@ -1,8 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //! uri -> nanti di pindah ke process.env
-const uri =
-  "mongodb+srv://raihank43:pakraden133@cluster0.bue4llj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {

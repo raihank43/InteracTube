5278d981288ba2ea158ab7a0f580d046a882ef59
raihank43 <raihankusuma5@gmail.com>
2024-03-31 16:04:50 +0700

    feat: remove cache when logout

--- client/src/screens/LogoutScreen.js
+++ client/src/screens/LogoutScreen.js
@@ -1,12 +1,17 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
 import * as SecureStore from "expo-secure-store";
+import { useApolloClient } from "@apollo/client";
+
 export default function LogoutScreen({ navigation }) {
   const { setIsSignedIn } = useContext(AuthContext);
+  const client = useApolloClient();
+
   useEffect(() => {
     // navigation.navigate("Login");
     async function clearToken() {
       await SecureStore.deleteItemAsync("accessToken");
+      await client.clearStore();
     }
 
     clearToken();

14d095ee13c40fe4b103e52ba26b7d3d55874cf5
raihank43 <raihankusuma5@gmail.com>
2024-03-31 19:33:33 +0700

    feat: follow user on search

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -1,9 +1,75 @@
+import { useMutation, useQuery } from "@apollo/client";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { GET_CURRENT_LOG_USER } from "../queries/GetUserProfile";
+import { GET_USER_BY_ID } from "../queries/GetUserById";
+import { FOLLOW_USER } from "../mutations/FollowMutation";
+import { GET_ALL_USERS } from "../queries/GetAllUsers";
 
 export default function ProfileCard({ Users }) {
+  const navigation = useNavigation();
+  const {
+    data: data,
+    loading: loading,
+    error: error,
+  } = useQuery(GET_CURRENT_LOG_USER);
+
+  const [followUser] = useMutation(FOLLOW_USER, {
+    refetchQueries: [GET_ALL_USERS],
+  });
+
+  if (loading) {
+    return (
+      <SafeAreaProvider>
+        <SafeAreaView
+          style={{
+            ...styles.container,
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          <ActivityIndicator size={"large"} />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    );
+  }
+
+  if (error) {
+    console.log(error);
+    return (
+      <SafeAreaProvider>
+        <SafeAreaView
+          style={{
+            ...styles.container,
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          <Text style={{ fontWeight: "bold", color: "white" }}>
+            Something went wrong:{" "}
+          </Text>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    );
+  }
+
+  const listTargetFollowers = Users.Followers;
+  const currentLogUserId = data.findCurrentLogUser._id;
+
+  const findCurrentLogUserOnTargetFollowerList = listTargetFollowers.find(
+    (obj) => obj.followerId.toString() === currentLogUserId
+  );
+
   return (
-    <TouchableOpacity style={styles.card}>
+    <TouchableOpacity
+      style={styles.card}
+      onPress={() => {
+        navigation.navigate("PeoplesProfile", { authorId: Users._id });
+      }}
+    >
       <Image
         style={styles.image}
         src="https://th.bing.com/th/id/OIP.WBjdfpIWhgt8n8WkzhOpJwHaKX?rs=1&pid=ImgDetMain"
@@ -16,9 +82,35 @@ export default function ProfileCard({ Users }) {
           {Users.Followings.length}
         </Text>
       </View>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Follow</Text>
-      </TouchableOpacity>
+      {findCurrentLogUserOnTargetFollowerList ? (
+        <TouchableOpacity style={styles.buttonUnfollow}>
+          <Text
+            style={styles.buttonTextUnfollow}
+            onPress={() => {
+              followUser({
+                variables: {
+                  followingId: Users._id,
+                },
+              });
+            }}
+          >
+            Unfollow
+          </Text>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          style={styles.buttonFollow}
+          onPress={() => {
+            followUser({
+              variables: {
+                followingId: Users._id,
+              },
+            });
+          }}
+        >
+          <Text style={styles.buttonTextFollow}>Follow</Text>
+        </TouchableOpacity>
+      )}
     </TouchableOpacity>
   );
 }
@@ -51,13 +143,22 @@ const styles = StyleSheet.create({
   followInfo: {
     fontSize: 12,
   },
-  button: {
+  buttonFollow: {
     padding: 10,
     backgroundColor: "red",
     borderRadius: 5,
   },
-  buttonText: {
+  buttonUnfollow: {
+    padding: 10,
+    backgroundColor: "black",
+    borderRadius: 5,
+  },
+  buttonTextFollow: {
     color: "#fff",
+    fontWeight: "bold",
+  },
+  buttonTextUnfollow: {
+    color: "white",
     fontWeight: "bold",
   },
 });
